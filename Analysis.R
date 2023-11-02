ec9c71926f8b8228a177fe5ab687da2191168751
sadrushi <sadrushil@gmail.com>
2023-11-02 11:30:33 +0530

    Bar graphs

--- Analysis.R
+++ Analysis.R
@@ -7,25 +7,31 @@ marks_data <- read_csv("Clean data set.csv")
 
 # Graphs
 
-# Bar chart
+# Bar chart of students attendance per lecture
 marks_data_g1 <- marks_data %>% select(9:15) %>% 
   pivot_longer(cols = 1:7, names_to = "Lecture.Number", values_to = "Attendance") %>%
   filter(Attendance == 1)
 
 ggplot(marks_data_g1, aes(x=Lecture.Number)) + geom_bar() + 
-  labs(title = "Students attendance per lecture", x = "Lecture number", 
+  labs(title = "Students attendance per lecture", x = "Lecture", 
        y = "Total attendace") + 
   geom_text(aes(label=after_stat(count)),stat='count',position=position_dodge(0.9),
             vjust=-0.2)
 
+# Bar chart of Total attendance
+ggplot(marks_data, aes(x=Total_Attendance)) + geom_bar() + 
+  labs(title = "Total count of student's attendance for lectures", 
+       x = "Number of lectures", 
+       y = "Total attendace") + 
+  geom_text(aes(label=after_stat(count)),stat='count',position=position_dodge(0.9),
+            vjust=-0.2) + scale_x_continuous(breaks = seq(0,7,1))
 
-# Histogram
 
-ggplot(marks_data) + geom_histogram(aes(Total_Attendance))
 
-ggplot(marks_data) + geom_histogram(aes(Mid_34))
 
-ggplot(marks_data) + geom_histogram(aes(Grand_Final))
+
+
+
 
 
 
